f3ffc9f8fc81d7b00617d0505ce3c4cd52cc0042
Rob Knight <raknight@ucsc.edu>
2019-12-16 17:09:50 -0800

    Clean up and style billboards

--- src/components/Billboards.vue
+++ src/components/Billboards.vue
@@ -4,10 +4,12 @@
       <li v-for="b in billboards" :key="b.index" :class="[b.layout, b.color]">
         <div class="text">
           <h1>{{ b.headline }}</h1>
-          <p v-html="b.teaser"></p>
-          <qrcode :value="b.link" :options="{ width: 200 }"></qrcode>
+          <div class="teaser">
+            <qrcode :value="b.link" :options="{ width: 80, margin: 2 }"></qrcode>
+            <p v-html="b.teaser"></p>
+          </div>
         </div>
-        <img :src="b.image" :alt="b.headline">
+        <img :src="b.image" :alt="b.headline" class="banner">
     </li>
   </ol>
   </div>
@@ -27,7 +29,7 @@ export default {
     };
   },
   mounted() {
-    axios({ method: "GET", "url": "billboards.json" }).then(result => {
+    axios({ method: "GET", "url": "data/billboards.json" }).then(result => {
       this.billboards = result.data.billboards;
     }, error => {
         console.error(error);
@@ -39,27 +41,80 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
+ol {
   list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  padding: 0 0 2rem 0;
+  margin: 0;
+  position: relative;
+  min-height: calc(100vw/3.2);
 }
 
-.container li {
+li {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
   width: 100%;
+  box-sizing: border-box;
+  position: absolute;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+}
+
+.banner {
+  height: auto;
+  width: 100%;
+  z-index: -999;
 }
 
 .text {
   position: absolute;
-  background-color: red;
+  padding: 1.2vw 4.4vw 1.2vw 2vw;
+  color: #fff;
+  background-color: rgba(81, 163, 189, 0.8);
+  width: 33rem;
+  font-size: 16px;
+  font-weight: 400;
+  z-index: 999;
+  text-align: left;
+  line-height: 1.3;
+  clip-path: polygon(0 0, 100% 0, 96% 100%, 0% 100%);
+
+  h1 {
+    margin: 0 0 .25rem 0;
+    font-size: 2.225rem;
+    font-weight: 400;
+    text-transform: uppercase;
+  }
+
+  p {
+    margin: .5rem 0 0;
+  }
+
+  canvas {
+    display: block;
+    float: right;
+    margin: 0 0 .25em 1em;
+  }
+  
+  .left & {
+    left: 0;
+    bottom: 2vh;
+  }
+
+  .right & {
+    right: 0;
+    bottom: 2vh;
+  }
+
+  .bottom & {
+    left: 0;
+    bottom: 2vh;
+    
+    .teaser {
+      max-width: 42rem;
+    }
+  }
 }
+
+
+
 </style>
