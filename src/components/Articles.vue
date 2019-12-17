7c35df053d9245a82c6eaaf746eef40e58c3ecc8
Rob Knight <raknight@ucsc.edu>
2019-12-17 13:17:15 -0800

    Reduce to 6 articles, remove title, fight widows, adjust QR code

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="articles">
-    <h1>Recent News</h1>
     <ul class="article-list">
-      <li v-for="i in articles" :key="i.index">        
-        <qrcode :value="i.url" :options="{ width: 80, margin: 2 }"></qrcode>
+      <li v-for="i in articles" :key="i.index">
+        <qrcode :value="i.url" :options="{ width: 80, margin: 2, color: { dark: '#333', light: '#fff' }}"></qrcode>
         <div class="text">
             <p v-html="i.date"></p>
-            <h2>{{ i.title }}</h2>
+            <h2 v-html="widows(i.title)"></h2>
         </div>
         <!-- <img :src="i.thumbnail" :alt="i.title" class="thumbnail"> -->
     </li>
@@ -29,12 +28,23 @@ export default {
   },
   mounted() {
     axios({ method: "GET", "url": "data/articles.json" }).then(result => {
-      this.articles = result.data.items.slice(0, 8);
+      this.articles = result.data.items.slice(0, 6);
     }, error => {
         console.error(error);
     });
   },
-  methods: {}
+  methods: {
+    // Method to add non-breaking space to text output
+    // https://css-tricks.com/preventing-widows-in-post-titles/
+    widows: function (title) {
+      var titleArray = title.split(" ");
+      if (titleArray.length > 1) {
+        titleArray[titleArray.length-2] += "&nbsp;" + titleArray[titleArray.length-1];
+        titleArray.pop();
+        return titleArray.join(" ");
+      }
+    }
+  }
 };
 </script>
 
@@ -43,8 +53,8 @@ export default {
 
 .articles {
     text-align: left;
-    padding: 0 1.2vw;
-    font-size: 1.4vw;
+    padding: 1vw 1.2vw;
+    font-size: 1.25vw;
 }
 
 ul {
@@ -52,9 +62,9 @@ ul {
   padding: 0;
   margin: 0;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-row-gap: 1em;
-  grid-column-gap: .5em;
+  grid-template-columns: repeat(3, 1fr);
+  grid-row-gap: 1.5em;
+  grid-column-gap: 3.2em;
   text-align: left;
 }
 
@@ -63,30 +73,38 @@ li {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-display: flex;
-flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 
 h1,
 h2 {
-    margin: 0 0 .25em 0;
-    font-size: 1.725em;
-    font-weight: 400;
-    color: #333;
+  margin: 0 0 .25em 0;
+  font-weight: 400;
+  color: #333;
+  line-height: 1.28;
+}
+
+h1 {
+  font-size: 2.6em;
+  text-align: center;
+  margin: .5vw 0 1vw;
+  font-weight: 400;
+  text-transform: uppercase;
 }
 
 h2 {
-    font-size: 1em;
+  font-size: 1.35em;
 }
 
 p {
-    margin: .28em 0 .25em;
-    font-size: .75em;
+  margin: 0 0 .25em;
+  font-size: .75em;
 }
 
 canvas {
-    display: block;
-    margin: 0 .5rem .25em 0;
+  display: block;
+  margin: 0 .5rem .25em 0;
 }
-  
+
 </style>
