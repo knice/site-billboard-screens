3b7811fa1f17b66ed50ae31015dec118977741df
Rob Knight <raknight@ucsc.edu>
2019-12-17 13:15:50 -0800

    Switch to Roboto Condensed

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ body {
 }
 
 #app {
-  font-family: "Roboto", Helvetica, Arial, sans-serif;
+  font-family: "Roboto Condensed", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
