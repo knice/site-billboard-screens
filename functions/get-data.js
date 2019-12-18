82820462c548faed95b46e319f042d38477ed266
Rob Knight <raknight@ucsc.edu>
2019-12-17 17:14:51 -0800

    Remove cheerio library

--- functions/get-data.js
+++ functions/get-data.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const cheerio = require("cheerio");
 const request = require("request");
 
 exports.handler = function(event, context, callback) {
@@ -9,7 +8,7 @@ exports.handler = function(event, context, callback) {
 
   // transform the size parameter into the image URL pattern needed in the request
   const option = {
-    billboards: "https://www.ucsc.edu/index.json",
+    billboards: "https://www.ucsc.edu/index.jsonp",
     articles: "https://news.ucsc.edu/latest.json"
   };
 
@@ -19,12 +18,10 @@ exports.handler = function(event, context, callback) {
   // Make the request to twitter and parse the response
   request(option[items], function(err, response, body) {
     if (!err && response.statusCode === 200) {
-      const $ = cheerio.load(body);
-      const fetchedItems = $;
       return callback(null, {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
-        body: fetchedItems
+        body: body
       });
     } else {
       return callback(null, {
